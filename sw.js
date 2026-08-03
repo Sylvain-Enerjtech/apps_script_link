a6ab26c4ad3a5e4cef896ae88e3d39066dacea41
Sylvain-Enerjtech <s.robillart@enerjtech.com>
2026-08-03 15:36:41 +0200

    Fix duplicate install event listener in sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,3 @@
 self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('fetch', (event) => {});self.addEventListener('install', (e) => self.skipWaiting());
 self.addEventListener('fetch', (event) => {});
